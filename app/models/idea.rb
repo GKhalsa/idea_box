1464aa2d89f599e33a0c31962442396e9c4b669a
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-29 15:07:23 -0600

    add case insensitive search

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,7 @@ class Idea < ActiveRecord::Base
   enum quality: %w(swill plausible genius)
 
   def self.filter_by_query(query)
-    downcase_query = query
-    Idea.where('title LIKE ? OR body LIKE ?', "%#{downcase_query}%","%#{downcase_query}%")
+    downcase_query = query.downcase
+    Idea.where('LOWER(title) LIKE ? OR LOWER(body) LIKE ?', "%#{downcase_query}%","%#{downcase_query}%")
   end
 end
