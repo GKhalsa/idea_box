58654016fe65951cb2052550c61ccb69c5e8b63f
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-28 21:38:24 -0600

    add editing of title and body

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :destroy, :update]
+      resources :edit, only: [:update]
     end
   end
 
